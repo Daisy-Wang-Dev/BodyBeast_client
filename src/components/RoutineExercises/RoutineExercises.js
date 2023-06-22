04b4360e51a89d7de71658cab009ed9d27d85eb7
Xueying Wang <daiszws19@gmail.com>
2023-06-22 10:47:13 +0100

    Onclick button adds new row for each exercise

--- src/components/RoutineExercises/RoutineExercises.js
+++ src/components/RoutineExercises/RoutineExercises.js
@@ -2,8 +2,23 @@ import "./RoutineExercises.scss";
 import RemoveIcon from "../../assets/icons/remove.svg";
 import CompleteIcon from "../../assets/icons/complete.svg";
 import AddIcon from "../../assets/icons/add.svg";
+import { useState } from "react";
 
 const RoutineExercises = () => {
+  const [arr, setArr] = useState([]);
+
+  const addInput = () => {
+    setArr((s) => {
+      return [
+        ...s,
+        {
+          type: "number",
+          value: "",
+        },
+      ];
+    });
+  };
+
   return (
     <section className="training">
       <form>
@@ -22,7 +37,6 @@ const RoutineExercises = () => {
           <input
             className="training__name"
             type="text"
-            id="name"
             name="name"
             value="lorem ipsum"
           />
@@ -35,18 +49,8 @@ const RoutineExercises = () => {
               <h4 className="training__input-header">add</h4>
             </div>
             <div className="training__input-fields">
-              <input
-                className="training__input"
-                type="number"
-                id="input"
-                name="input"
-              />
-              <input
-                className="training__input"
-                type="number"
-                id="input"
-                name="input"
-              />
+              <input className="training__input" type="number" name="input" />
+              <input className="training__input" type="number" name="input" />
               <div className="training__icon">
                 <img src={CompleteIcon} alt="Completed exercise" />
               </div>
@@ -54,9 +58,38 @@ const RoutineExercises = () => {
                 <img src={RemoveIcon} alt="Remove exercise" />
               </div>
               <div className="training__icon">
-                <img src={AddIcon} alt="Remove exercise" />
+                <img onClick={addInput} src={AddIcon} alt="Remove exercise" />
               </div>
             </div>
+            {arr.map((element) => {
+              return (
+                <div className="training__input-fields">
+                  <input
+                    className="training__input"
+                    type="number"
+                    name="input"
+                  />
+                  <input
+                    className="training__input"
+                    type="number"
+                    name="input"
+                  />
+                  <div className="training__icon">
+                    <img src={CompleteIcon} alt="Completed exercise" />
+                  </div>
+                  <div className="training__icon">
+                    <img src={RemoveIcon} alt="Remove exercise" />
+                  </div>
+                  <div className="training__icon">
+                    <img
+                      onClick={addInput}
+                      src={AddIcon}
+                      alt="Remove exercise"
+                    />
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </article>
         <button type="submit">SMASHED</button>
