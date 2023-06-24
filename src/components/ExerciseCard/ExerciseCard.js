8d1ab63a55cfc1ddca2de97129b6073ccfc560ab
Xueying Wang <daiszws19@gmail.com>
2023-06-24 18:23:28 +0100

    Recreate exerciseCard component on RoutineExercise Page

--- src/components/ExerciseCard/ExerciseCard.js
+++ src/components/ExerciseCard/ExerciseCard.js
@@ -3,42 +3,46 @@ import CompleteIcon from "../../assets/icons/complete.svg";
 import AddIcon from "../../assets/icons/add.svg";
 import { useState } from "react";
 import "./ExerciseCard.scss";
+import { Formik, Field, ErrorMessage, FieldArray } from 'formik';
 
-const ExerciseCard = ({ exercise }) => {
+const ExerciseCard = ({ index, exercise }) => {
   //   // Add one more set
-  const [counter, setCounter] = useState(1);
+//   const [counter, setCounter] = useState(1);
 
-  const handleAddClick = () => {
-    setCounter(counter + 1);
-  };
+//   const handleAddClick = () => {
+//     setCounter(counter + 1);
+//   };
 
-  //Completed a set
-  const [isCompletedRow, setCompletedRow] = useState({});
+//   //Completed a set
+//   const [isCompletedRow, setCompletedRow] = useState({});
 
-  const handleCompleteClick = (index) => {
-    setCompletedRow({ ...isCompletedRow, [index]: !isCompletedRow[index] });
-  };
+//   const handleCompleteClick = (index) => {
+//     setCompletedRow({ ...isCompletedRow, [index]: !isCompletedRow[index] });
+//   };
 
-  //Remove a set
-  const handleRemoveClick = () => {
-    setCounter(counter - 1);
-  };
+//   //Remove a set
+//   const handleRemoveClick = () => {
+//     setCounter(counter - 1);
+//   };
 
- //Handle input values
-  const [trainingName, setTrainingName] = useState(exercise);
-  const handletrainingNameChange = (e) => {
-    setTrainingName(e.target.value);
-  };
+//  //Handle input values
+//   const [trainingName, setTrainingName] = useState(exercise);
+//   const handletrainingNameChange = (e) => {
+//     setTrainingName(e.target.value);
+//   };
+console.log(index);
+console.log(exercise);
 
   return (
     <article className="training__exercise">
+      {/* Change to field */}
       <input
         className="training__name"
         type="text"
-        name="name"
+        name={`${exercise}.${index}.exercise_name`}
         placeholder="New Exercise"
-        onChange={handletrainingNameChange}
-        value={trainingName}
+        // onChange={handletrainingNameChange}
+        // value={trainingName}
       />
       <div className="training__inputs">
         <div className="training__input-headers">
@@ -48,7 +52,7 @@ const ExerciseCard = ({ exercise }) => {
           <h4 className="training__input-header">remove</h4>
           <h4 className="training__input-header">add</h4>
         </div>
-        {Array.from(Array(counter).keys()).map((count, index) => {
+        {/* {Array.from(Array(counter).keys()).map((count, index) => {
           return (
             <div
               key={index}
@@ -83,9 +87,9 @@ const ExerciseCard = ({ exercise }) => {
                   alt="Remove exercise"
                 />
               </div>
-            </div>
-          );
-        })}
+            </div> */}
+          {/* );
+        })} */}
       </div>
     </article>
   );
--- src/components/RoutineExercises/RoutineExercises.js
+++ src/components/RoutineExercises/RoutineExercises.js
@@ -7,12 +7,14 @@ import { Link } from "react-router-dom";
 import { Formik, Field, Form, ErrorMessage, FieldArray } from "formik";
 import TryNewExerciseCard from "../../components/TryNewExerciseCard/TryNewExerciseCard";
 import TryExerciseCard from "../../components/TryExerciseCard/TryExerciseCard";
+import RemoveIcon from "../../assets/icons/remove.svg";
+import CompleteIcon from "../../assets/icons/complete.svg";
+import AddIcon from "../../assets/icons/add.svg";
 
 const RoutineExercises = ({ routineId }) => {
   const [routineDetails, setRoutineDetails] = useState(null);
   const [routineName, setRoutineName] = useState("");
   const [exercises, setExercises] = useState([]);
-  
 
   const getRoutineDetails = async () => {
     try {
@@ -20,16 +22,14 @@ const RoutineExercises = ({ routineId }) => {
         process.env.REACT_APP_API_URL + `/user/1/routine/${routineId}/exercises`
       );
       const { name, exercises } = response.data;
-     
+
       setRoutineName(name);
       setExercises(exercises);
       setRoutineDetails(response.data);
-    
     } catch (err) {
       console.log(`Error: ${err.message}`);
     }
   };
-
 
   useEffect(() => {
     getRoutineDetails();
@@ -50,13 +50,15 @@ const RoutineExercises = ({ routineId }) => {
     return <h1 className="loading">Data Loading</h1>;
   }
 
-
   return (
     <section className="training">
       <Formik
         initialValues={{
           routineName: routineName,
-          exercises: exercises.map((exercise) => ({name:exercise, sets:[{ weight: "", reps: "" }]})),
+          exercises: exercises.map((exercise) => ({
+            exercise_name: exercise,
+            sets: [{ weight: "", reps: "" }],
+          })),
           newExercises: [].map((c) => ({
             name: "",
             sets: [{ weight: "", reps: "" }],
@@ -67,7 +69,7 @@ const RoutineExercises = ({ routineId }) => {
           console.log(JSON.stringify(values, null, 2));
         }}
       >
-        {({values}) => (
+        {({ values }) => (
           <Form className="training__form">
             <div className="training__title-container">
               <Field
@@ -82,7 +84,79 @@ const RoutineExercises = ({ routineId }) => {
                 {() => (
                   <>
                     {values.exercises.map((exercise, index) => (
-                      <ExerciseCard key={index} exercise={exercise.name} />
+                      // <ExerciseCard
+                      //   key={index}
+                      //   index={index}
+                      //   exercise={exercise}
+                      // />
+                      <article key={index} className="training__exercise">
+                        <Field
+                          className="training__name"
+                          name={`exercises.${index}.exercise_name`}
+                          placeholder="New Exercise"
+                          type="text"
+                        />
+                        <ErrorMessage
+                          name={`exercises.${index}.exercise_name`}
+                          component="div"
+                          className="field-error"
+                        />
+                        <div className="training__inputs">
+                          <div className="training__input-headers">
+                            <h4 className="training__input-header">kg</h4>
+                            <h4 className="training__input-header">reps</h4>
+                            <h4 className="training__input-header">
+                              completed
+                            </h4>
+                            <h4 className="training__input-header">remove</h4>
+                            <h4 className="training__input-header">add</h4>
+                          </div>
+                          <FieldArray name={`exercises.${index}.sets`}>
+                            {({ remove, push }) => (
+                              <>
+                                {values.exercises[index].sets.map((_, idx) => (
+                                  <div
+                                    key={idx}
+                                    className="training__input-fields"
+                                  >
+                                    <Field
+                                      className="training__input"
+                                      type="number"
+                                      name={`exercises.${index}.sets.${idx}.weight`}
+                                    />
+                                    <Field
+                                      className="training__input"
+                                      type="number"
+                                      name={`exercises.${index}.sets.${idx}.reps`}
+                                    />
+                                    <div className="training__icon">
+                                      <img
+                                        className="training__completed"
+                                        src={CompleteIcon}
+                                        alt="Completed exercise"
+                                      />
+                                    </div>
+                                    <div className="training__icon">
+                                      <img
+                                        src={RemoveIcon}
+                                        alt="Remove exercise"
+                                        onClick={()=>remove({ weight: "", reps: "" })}
+                                      />
+                                    </div>
+                                    <div className="training__icon">
+                                      <img
+                                        src={AddIcon}
+                                        alt="Remove exercise"
+                                        onClick={()=>push({ weight: "", reps: "" })}
+                                      />
+                                    </div>
+                                  </div>
+                                ))}
+                              </>
+                            )}
+                          </FieldArray>
+                        </div>
+                      </article>
                     ))}
                   </>
                 )}
@@ -94,12 +168,13 @@ const RoutineExercises = ({ routineId }) => {
                       <NewExerciseCard key={index} index={index} />
                     ))}
                     <h3
-                    className="training__add"
-                    onClick={() => push({ name: "", sets: [{ weight: "", reps: "" }] })}
-                  >
-                    + EXERCISE
-                  </h3>
-                   
+                      className="training__add"
+                      onClick={() =>
+                        push({ name: "", sets: [{ weight: "", reps: "" }] })
+                      }
+                    >
+                      + EXERCISE
+                    </h3>
                   </>
                 )}
               </FieldArray>
