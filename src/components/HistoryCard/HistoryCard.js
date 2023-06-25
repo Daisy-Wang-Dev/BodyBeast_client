fb66cd9c9fb9659418c960878e44db5023a53a37
Xueying Wang <daiszws19@gmail.com>
2023-06-26 00:52:26 +0100

    Get history data from api

--- src/components/HistoryCard/HistoryCard.js
+++ src/components/HistoryCard/HistoryCard.js
@@ -2,18 +2,21 @@ import "./HistoryCard.scss";
 import ChevronIcon from "../../assets/icons/chevron.svg";
 import { Link } from "react-router-dom";
 
-
-const HistoryCard = () => {
+const HistoryCard = ({ history }) => {
+  const { id, name, created_at } = history;
   return (
     <article className="history">
       <div className="history__title-container">
-        <h2 className="history__title">title</h2>
-        <h3 className="history__date">date</h3>
+        <h2 className="history__title">{name}</h2>
+        <h3 className="history__date">{created_at}</h3>
       </div>
-      <Link to="" className="history__link">
-        <img className="history__icon" src={ChevronIcon} alt="Link to see history details" />
+      <Link to={`/histories/${id}`} className="history__link">
+        <img
+          className="history__icon"
+          src={ChevronIcon}
+          alt="Link to see history details"
+        />
       </Link>
-
     </article>
   );
 };
--- src/pages/History/History.js
+++ src/pages/History/History.js
@@ -1,12 +1,38 @@
 import "./History.scss";
 import HistoryCard from "../../components/HistoryCard/HistoryCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const History = () => {
+  const [histories, setHistories] = useState(null);
+
+  const getHistories = async () => {
+    try {
+      const response = await axios.get(
+        process.env.REACT_APP_API_URL + "/user/1/history"
+      );
+      console.log(response.data);
+      setHistories(response.data);
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  };
+
+  useEffect(() => {
+    getHistories();
+  }, []);
+
+  if (!histories) {
+    return;
+  }
+
   return (
     <main>
       <section className="histories">
         <h1 className="histories__title">HISTROY</h1>
-        <HistoryCard />
+        {histories.map((history) => (
+          <HistoryCard key={history.id} history={history} />
+        ))}
       </section>
     </main>
   );
