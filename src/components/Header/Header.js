b8e034e96265bacd966887ae7f5c619534773185
Xueying Wang <daiszws19@gmail.com>
2023-06-25 23:11:27 +0100

    Display user info on header dynamically

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,42 @@
+import { useEffect, useState } from "react";
 import "./Header.scss";
+import axios from "axios";
 
 const Header = () => {
-    // Need a user prop to render username dynamically
+  const [userInfo, setUserInfo] = useState(null);
+  const [username, setUsername] = useState("");
+  const [mode, setMode] = useState("");
+  const [dayDiff, setDayDiff] = useState(0);
+
+  // Today
+  const Today = new Date();
+  // Mode start day
+  let startDay;
+
+  const getUserInfo = async () => {
+    try {
+      const response = await axios.get(
+        process.env.REACT_APP_API_URL + "/user/1"
+      );
+      const { user_name, mode, updated_at } = response.data;
+      setUserInfo(response.data);
+      setUsername(user_name);
+      setMode(mode);
+      const dayDifference = Math.ceil((Today.getTime() - new Date(updated_at).getTime()) /
+      (1000 * 60 * 60 * 24));
+      setDayDiff(dayDifference);
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  };
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
   return (
     <header className="header">
-      <h1 className="header__title">WELCOME BACK,"Daisy12"</h1>
+      <h1 className="header__title">WELCOME BACK, {username}</h1>
+      <h2 className="header__subtitle">You're {dayDiff} days into {mode}.</h2>
     </header>
   );
 };
