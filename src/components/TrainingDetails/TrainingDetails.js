e280edc62aa61bb07c14104e2e6c598121364d1b
Xueying Wang <daiszws19@gmail.com>
2023-06-26 10:21:20 +0100

    Get exercise data from api

--- src/components/TrainingDetails/TrainingDetails.js
+++ src/components/TrainingDetails/TrainingDetails.js
@@ -1,24 +1,32 @@
 import "./TrainingDetails.scss";
 
-const TrainingDetails = () => {
+const TrainingDetails = ({ historyDetails }) => {
+  const { name, created_at, exercises } = historyDetails;
   return (
     <article className="history-details__card">
       <div className="history-details__title-container">
-        <h1 className="history-details__title">title</h1>
-        <h2 className="history-details__date">date</h2>
-      </div> 
-      {/* use  map here */}
-      <div className="history-details__exercise-container">
-        <h2 className="history-details__exercise">exercisename</h2>
-        <div className="history-details__container">
-          <h4 className="history-details__table-header">weight</h4>
-          <h4 className="history-details__table-header">rep</h4>
-          <h4 className="history-details__table-header">volume</h4>
-
-        </div>
-        {/* use .map here */}
-
+        <h1 className="history-details__title">{name}</h1>
+        <h2 className="history-details__date">{created_at}</h2>
       </div>
+      {exercises.map((exercise, index) => (
+        <div key={index} className="history-details__exercise-container">
+          <h2 className="history-details__exercise">
+            {exercise.exercise_name}
+          </h2>
+          <div className="history-details__container">
+            <h4 className="history-details__table-header">weight</h4>
+            <h4 className="history-details__table-header">rep</h4>
+            <h4 className="history-details__table-header">volume</h4>
+          </div>
+          {exercise.sets.map((set, idx) => (
+            <div key={idx} className="history-details__data">
+              <p className="history-details__table-header">{set.weight}</p>
+              <p className="history-details__table-header">{set.rep}</p>
+              <p className="history-details__table-header">{set.volume}</p>
+            </div>
+          ))}
+        </div>
+      ))}
 
     </article>
   );
--- src/pages/HistoryDetails/HistoryDetails.js
+++ src/pages/HistoryDetails/HistoryDetails.js
@@ -2,16 +2,36 @@ import { useParams } from "react-router-dom";
 import "./HistoryDetails.scss";
 import TrainingDetails from "../../components/TrainingDetails/TrainingDetails";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 const HistoryDetails = () => {
   const { routineId } = useParams();
-  console.log(routineId);
 
+  const [historyDetails, setHistoryDetails] = useState(null);
+
+  const getHistoryDetails = async () => {
+    try {
+      const response = await axios.get(
+        process.env.REACT_APP_API_URL + `/user/1/history/${routineId}`
+      );
+      setHistoryDetails(response.data);
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  };
+
+  useEffect(() => {
+    getHistoryDetails();
+  }, []);
+
+  if (!historyDetails) {
+    return <h1 className="loading">DATA LOADING...</h1>;
+  }
 
   return (
     <main>
       <section className="history-details">
-        <TrainingDetails />
+        <TrainingDetails historyDetails={historyDetails}/>
       </section>
     </main>
   );
